a421b10319e0098a83bfa79fb9a74b9f1d62ff41
sicness9 <30636804+sicness9@users.noreply.github.com>
2022-04-28 14:35:10 -0500

    - Lay groundwork to start creating the user home page

---
+++ src/components/SignedInUserHome/SignedInUserHome.jsx
@@ -0,0 +1,12 @@
+// components
+import UserHomeGrid from "../UserHomeGrid/UserHomeGrid";
+
+const SignedInUserHome = () => {
+  return (
+    <div>
+      <UserHomeGrid />
+    </div>
+  );
+};
+
+export default SignedInUserHome;
