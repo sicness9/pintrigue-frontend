c28cffc8b51b7f75ad636aa8f763d53bdbdfe009
sicness9 <30636804+sicness9@users.noreply.github.com>
2022-04-28 14:34:42 -0500

    - Add background darken for modal - Position for modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,62 +1,63 @@
 // Modal component
 
-import { useRef, useState, useEffect } from "react"
-import { useSpring, animated } from "react-spring"
+import { useRef, useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
 
-import { AnimatedModal, Content } from "./Modal.styles"
+import { AnimatedModal, Content } from "./Modal.styles";
 
-import logo from "../../images/logo.svg"
+import logo from "../../images/logo.svg";
 
 const Modal = (props) => {
-    const ref = useRef(null)
+  const ref = useRef(null);
 
-    
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-          if (props.open && ref.current && !ref.current.contains(event.target)) {
-              props.setOpen(false);
-          }
-        }
-  
-          document.addEventListener("click", handleClickOutside);
-  
-          return () => {
-              document.removeEventListener("click", handleClickOutside);
-          }
-    }, [props, props.open])
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (props.open && ref.current && !ref.current.contains(event.target)) {
+        props.setOpen(false);
+      }
+    };
 
-    const animateModal = useSpring({ opacity: props.open ? 1 : 0, duration: 800 })
+    document.addEventListener("click", handleClickOutside);
 
-    return (
-        <Content>
-            <div className="modal" ref={ref}>
-            {props.open &&
-                <AnimatedModal style={animateModal}>
-                    <animated.div className="modal-box">
-                        <div className="modal-top-content">
-                            <img className="-modal-logo" src={logo} alt="logo" />
-                            <h1>Welcome back, User!</h1>
-                            <h2>Image will be there</h2>
-                            <button>Log in</button>
-                        </div>
-                        <div>
-                            <hr/>
-                        </div>
-                        <div className="modal-bottom-content">
-                            <div className="login-switch-account">
-                                <a href="#">Not you? Log in with different account</a>
-                            </div>
-                            <div className="login-sign-up">
-                                <a href="#">Don't have an account? Sign up</a>
-                            </div>
-                        </div>
-                    </animated.div>
-                </AnimatedModal>
-            }
-            </div>
-        </Content>
-        
-    )
-}
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [props, props.open]);
 
-export default Modal
+  const animateModal = useSpring({
+    opacity: props.open ? 1 : 0,
+    duration: 800,
+  });
+
+  return (
+    <Content>
+      {props.open && (
+        <div className="modal">
+          <AnimatedModal style={animateModal} ref={ref}>
+            <animated.div className="modal-box">
+              <div className="modal-top-content">
+                <img className="-modal-logo" src={logo} alt="logo" />
+                <h1>Welcome back, User!</h1>
+                <h2>Image will be there</h2>
+                <button>Log in</button>
+              </div>
+              <div>
+                <hr />
+              </div>
+              <div className="modal-bottom-content">
+                <div className="login-switch-account">
+                  <a href="#">Not you? Log in with different account</a>
+                </div>
+                <div className="login-sign-up">
+                  <a href="#">Don't have an account? Sign up</a>
+                </div>
+              </div>
+            </animated.div>
+          </AnimatedModal>
+        </div>
+      )}
+    </Content>
+  );
+};
+
+export default Modal;
--- src/components/Modal/Modal.styles.js
+++ src/components/Modal/Modal.styles.js
@@ -1,82 +1,95 @@
-import styled from 'styled-components';
-import { animated } from 'react-spring';
+import styled from "styled-components";
+import { animated } from "react-spring";
 
+export const Wrapper = styled.div`
+  box-sizing: border-box;
+  display: block;
+`;
 
 export const Content = styled.div`
-    .modal {
-        background-color: rgba(0, 0, 0, 0.5);
-    }
-`
+  box-sizing: border-box;
+  display: block;
+
+  .modal {
+    z-index: 1000;
+    background-color: rgba(0, 0, 0, 0.6);
+    opacity: 1;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    position: fixed;
+  }
+`;
 
 export const AnimatedModal = styled(animated.div)`
-    position: absolute;
-    width: 375px;
-    height: 400px;
-    padding: 32px 0px;
-    margin: auto;
-    top: 50%;
-    left: 50%;
-    z-index: 999;
-    transform: translate(-50%, -50%);
-    width: 400;
-    background-color: #FFFFFF;
-    border: 0.5px solid black;
-    border-radius: 8px;
-    box-shadow: rgb(0, 0, 0, 0.5) 0px 2px 10px;
-    padding: 4;
-    text-align: center;
-    
+  position: absolute;
+  width: 375px;
+  height: 400px;
+  padding: 32px 0px;
+  margin: auto;
+  top: 50%;
+  left: 50%;
+  z-index: 999;
+  transform: translate(-50%, -50%);
+  width: 400;
+  background-color: #ffffff;
+  border: 0.5px solid black;
+  border-radius: 8px;
+  box-shadow: rgb(0, 0, 0, 0.5) 0px 2px 10px;
+  padding: 4;
+  text-align: center;
 
-    .modal-top-content {
-        width: 100%; 
-        height: 325px;
+  .modal-top-content {
+    width: 100%;
+    height: 325px;
 
-        img {
-            margin-bottom: 12px;
-            height: 50px;
-        }
-
-        h1 {
-            margin-bottom: 12px;
-        }
-
-        h2 {
-            margin-bottom: 12px;
-        }
-
-        button {
-            width: 80%;
-            height: 40px;
-            border-radius: 20px;
-            border: none;
-            margin-bottom: 12px;
-            background-color: var(--primary-color);
-            color: white;
-            font-weight: bold;
-        }
+    img {
+      margin-bottom: 12px;
+      height: 50px;
     }
-    
-    .modal-bottom-content {
-        height: 100px;
-        display: flex;
-        flex-direction: column;
-        gap: 5px;
-        margin-top: 17px;
 
-        .login-switch-account {
-            margin-top: 5px;
-        }
-
-        a {
-            text-decoration: none;
-            font-style: none;
-            margin-top: 5px;
-            color: black;
-            font-weight: bold;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
+    h1 {
+      margin-bottom: 12px;
     }
-`
+
+    h2 {
+      margin-bottom: 12px;
+    }
+
+    button {
+      width: 80%;
+      height: 40px;
+      border-radius: 20px;
+      border: none;
+      margin-bottom: 12px;
+      background-color: var(--primary-color);
+      color: white;
+      font-weight: bold;
+    }
+  }
+
+  .modal-bottom-content {
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    margin-top: 17px;
+
+    .login-switch-account {
+      margin-top: 5px;
+    }
+
+    a {
+      text-decoration: none;
+      font-style: none;
+      margin-top: 5px;
+      color: black;
+      font-weight: bold;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  }
+`;
