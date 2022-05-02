ff6fadda56b368a8f4ab75773fb317bd84cedf51
sicness9 <30636804+sicness9@users.noreply.github.com>
2022-05-02 16:50:42 -0500

    - Add AuthedHeader component to this component - Reformat code to better fit the addition of another component

--- src/components/SignedInUserHome/SignedInUserHome.jsx
+++ src/components/SignedInUserHome/SignedInUserHome.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 // components
 import UserHomeGrid from "../UserHomeGrid/UserHomeGrid";
+import AuthedHeader from "../AuthedHeader/AuthedHeader";
+
+//styles
+import { Wrapper } from "./SignedInUserHome.styles";
 
 const SignedInUserHome = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>
-      <UserHomeGrid />
-    </div>
+    <>
+      <AuthedHeader />
+      <Wrapper>
+        <UserHomeGrid />
+      </Wrapper>
+    </>
   );
 };
 
