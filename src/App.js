81d888a52f68877fd4973852fc2a628cabbd87ad
sicness9 <30636804+sicness9@users.noreply.github.com>
2022-05-02 16:46:46 -0500

    - Remove header component from App.js and moved elsewhere

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // components
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import AuthedHeader from "./components/AuthedHeader/AuthedHeader";
+import SignedInUserHome from "./components/SignedInUserHome/SignedInUserHome";
 
 // styles
 import { GlobalStyles } from "./GlobalStyles";
@@ -12,9 +15,20 @@ function App() {
 
   return (
     <div className="App">
-      <GlobalStyles />
-      <Header />
-      <Main />
+      <Router>
+        {/* <Header /> */}
+        {/* <AuthedHeader /> */}
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/user_home"
+            element={<SignedInUserHome />}
+            isLoading={isLoading}
+            setIsLoading={setIsLoading}
+          />
+        </Routes>
+        <GlobalStyles />
+      </Router>
     </div>
   );
 }
