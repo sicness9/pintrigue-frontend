5236430ec59036bad4cb0121f81a5dcc1637b304
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-06-11 22:33:19 -0500

    - Create component and styles

---
+++ src/components/PinForFeed/PinForFeex.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const FeedPin = styled.div`
+  background-color: #e2e2e2;
+  height: 100%;
+  width: 100%;
+  margin-left: 15px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 12px;
+  cursor: pointer;
+
+  .image-container {
+    .obj {
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      border-radius: 8px;
+      overflow: hidden;
+    }
+  }
+`;
