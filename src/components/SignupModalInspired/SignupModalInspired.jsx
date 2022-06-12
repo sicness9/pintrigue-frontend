9ea1f2e7a82c8b84192f4f8eb8b87ab2bce6cf2d
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-06-11 22:36:10 -0500

    - Moved code to newly created component ModalSignupView

--- src/components/SignupModalInspired/SignupModalInspired.jsx
+++ src/components/SignupModalInspired/SignupModalInspired.jsx
@@ -1,7 +1,8 @@
+// components
+import ModalSignUpView from "../ModalSignupView/ModalSignupView";
+
 // styles
 import { SignupModalInspiredStyle } from "./SignupModalInspired.styles";
-
-import logo from "../../images/logo.svg";
 
 const SignupModalInspired = () => {
   return (
@@ -13,108 +14,8 @@ const SignupModalInspired = () => {
               <div className="modal-message-container">
                 <h2 className="modal-message">Sign up to get your ideas</h2>
               </div>
-
               <div className="signup-default-modal">
-                <div className="modal-box">
-                  <div className="modal-logo-container">
-                    <img
-                      className="modal-logo"
-                      alt="modal-logo"
-                      src={logo}
-                    ></img>
-                  </div>
-                  <div className="modal-title-container">
-                    <h1 className="modal-title">Welcome to Pintrigue</h1>
-                  </div>
-                  <div className="container-for-sub-title">
-                    <div className="sub-title-container">
-                      <div className="sub-title">Find new ideas to try</div>
-                    </div>
-                  </div>
-                  <div className="signup-options">
-                    <div className="form-container">
-                      <div className="form-container2">
-                        <form className="register-form">
-                          <div className="emailInputField">
-                            <fieldset>
-                              <div className="email-label-container">
-                                <label className="email-label" htmlFor="email">
-                                  Continue with email
-                                </label>
-                              </div>
-                              <span>
-                                <div className="email-input-container">
-                                  <input
-                                    autoComplete="email"
-                                    className="email-input"
-                                    id="email"
-                                    name="id"
-                                    placeholder="Email"
-                                    type="email"
-                                  ></input>
-                                </div>
-                              </span>
-                            </fieldset>
-                          </div>
-                          <div className="passwordInputField">
-                            <fieldset>
-                              <div className="password-label-container">
-                                <label
-                                  className="password-label"
-                                  htmlFor="password"
-                                >
-                                  Create a password
-                                </label>
-                              </div>
-                              <span>
-                                <div className="password-input-container">
-                                  <input
-                                    autoComplete="new-password"
-                                    className="password-input"
-                                    id="password"
-                                    name="password"
-                                    placeholder="Create a password"
-                                    type="password"
-                                  ></input>
-                                </div>
-                              </span>
-                            </fieldset>
-                          </div>
-                          <div className="ageInputField">
-                            <fieldset>
-                              <div className="age-label-container">
-                                <label className="age-label" htmlFor="age">
-                                  Enter your name
-                                </label>
-                              </div>
-                              <span>
-                                <div className="age-input-container">
-                                  <input
-                                    className="age-input"
-                                    id="age"
-                                    name="age"
-                                    placeholder="Age"
-                                  ></input>
-                                </div>
-                              </span>
-                            </fieldset>
-                          </div>
-                          <div className="registerFormSubmitButton">
-                            <button className="signup-button" type="submit">
-                              Continue
-                            </button>
-                          </div>
-                        </form>
-                        <p className="other-options">OR</p>
-                        <div className="active-account-container">
-                          <button className="already-have-account-button">
-                            Already a member? Log in
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ModalSignUpView />
               </div>
             </div>
           </div>
