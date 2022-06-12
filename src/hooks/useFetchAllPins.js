a265d4a608e3846d922439989a5c39c2ad403bbb
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-06-11 22:38:29 -0500

    - Create hook for fetching all pins

---
+++ src/hooks/useFetchAllPins.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useFetchAllPins = () => {
+  const [pins, setPins] = useState([]);
+
+  useEffect(() => {
+    let APICall = true;
+    if (APICall) {
+      axios
+        .get("http://127.0.0.1:8000/api/pins/popular/", {
+          params: { limit: 50 },
+        })
+        .then((res) => {
+          setPins(res.data);
+        });
+    }
+    return () => {
+      APICall = false;
+    };
+  }, []);
+
+  return pins;
+};
