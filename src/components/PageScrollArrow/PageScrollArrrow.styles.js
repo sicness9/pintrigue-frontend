fcfb0f852da28cd9e3d551e6284ae2db112e6142
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-06-11 22:31:10 -0500

    - Adjust left property in page-scroll-arrow class in CSS

--- src/components/PageScrollArrow/PageScrollArrrow.styles.js
+++ src/components/PageScrollArrow/PageScrollArrrow.styles.js
@@ -2,26 +2,29 @@ import styled from "styled-components";
 import { animated } from "react-spring";
 
 export const PageScrollArrowStyle = styled(animated.div)`
-    .page-scroll-arrow {
-        z-index: 1;
-        pointer-events: none;
-        position: fixed;
-        border-radius: 50%;
-        border-style: none;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 50px;
-        width: 50px;
-        outline: none;
-        cursor: pointer;
-        left: calc(50% - 25px);
-        bottom: 32px;
+  left: 0;
 
-        .img {
-            height: 24px;
-            width: 24px;
-            display: block;
-        }
+  .page-scroll-arrow {
+    z-index: 1;
+    pointer-events: none;
+    position: fixed;
+    border-radius: 50%;
+    border-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    width: 50px;
+    outline: none;
+    cursor: pointer;
+    /* left: calc(50% - 25px); */
+    bottom: 32px;
+    left: 50%;
+
+    .img {
+      height: 24px;
+      width: 24px;
+      display: block;
     }
-`
+  }
+`;
