7cb534c5ea1fda2cec44d4dbf22975ad40d52d03
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-06-11 22:18:46 -0500

    - Add user provider

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // components
-import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import AuthedHeader from "./components/AuthedHeader/AuthedHeader";
 import SignedInUserHome from "./components/SignedInUserHome/SignedInUserHome";
+import PinBuilder from "./components/PinBuilder/PinBuilder";
+import SearchResultPage from "./components/SearchResultPage/SearchResultPage";
+import PinView from "./components/PinView/PinView";
+
+// context
+import SuggestionContextProvider from "./Context/SuggestionContext";
+import CreatePinContextProvider from "./Context/newPinContext";
+import UserProvider, { UserContext } from "./Context/userContext";
 
 // styles
 import { GlobalStyles } from "./GlobalStyles";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const authed = useContext(UserContext);
 
   return (
     <div className="App">
       <Router>
-        {/* <Header /> */}
-        {/* <AuthedHeader /> */}
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route
-            path="/user_home"
-            element={<SignedInUserHome />}
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-          />
-        </Routes>
-        <GlobalStyles />
+        <UserProvider>
+          <CreatePinContextProvider>
+            <SuggestionContextProvider>
+              <Routes>
+                <Route path="/" element={<Main />} />
+                {authed && (
+                  <Route
+                    path="/user-home"
+                    element={<SignedInUserHome />}
+                    isLoading={isLoading}
+                    setIsLoading={setIsLoading}
+                  />
+                )}
+                {authed && (
+                  <Route path="/pin-builder" element={<PinBuilder />} />
+                )}
+                {authed && (
+                  <Route path="/search/pins" element={<SearchResultPage />} />
+                )}
+                {authed && <Route path="/pin/:id" element={<PinView />} />}
+              </Routes>
+            </SuggestionContextProvider>
+          </CreatePinContextProvider>
+          <GlobalStyles />
+        </UserProvider>
       </Router>
     </div>
   );
