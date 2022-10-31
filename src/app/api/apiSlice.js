58362b07b47c08e50ad2f93a8ec52b500c330689
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-10-31 10:18:40 -0400

    create api slice for redux

---
+++ src/app/api/apiSlice.js
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials, logOut } from "../../slices/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:8000/api",
+  // credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().token.value;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  //   if (result?.error?.originalStatus === 401) {
+  //     console.log("sending refresh token");
+  //     // send refresh token to get new access token
+  //     const refreshResult = await baseQuery("/refresh", api, extraOptions);
+  //     console.log(refreshResult);
+  //     if (refreshResult?.data) {
+  //       const user = api.getState().auth.user;
+  //       // store new token
+  //       api.dispatch(setCredentials({ ...refreshResult.data, user }));
+  //       // retry original query with new access token
+  //       result = await baseQuery(args, api, extraOptions);
+  //     } else {
+  //       api.dispatch(logOut());
+  //     }
+  //   }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({}),
+});
