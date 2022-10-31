105ceaddecc618a100fc17da4b517cd0df2afbae
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-10-31 10:18:27 -0400

    - add redux persist - add persist gate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
