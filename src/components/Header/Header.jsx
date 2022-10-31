48ebbd0e43a7bf9fb24c8e1f468ab3b6e3ecd598
nelson-mileiq <92046160+NelsonTorres9@users.noreply.github.com>
2022-10-31 10:20:06 -0400

    forward to main page when clicking logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,9 @@ const Header = () => {
       <Content>
         <nav className="nav-bar">
           <div className="logo-nav">
-            <img className="logo" src={logo} alt="logo" />
+            <a href="/">
+              <img className="logo" src={logo} alt="logo" />
+            </a>
             <h1>Pintrigue</h1>
           </div>
           <div className="header-btn-container">
