c6e2487962121de4b4885db6c6a33e2552782679
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-03-29 15:59:56 -0400

    remove unused components from page

--- src/components/userSettings/EditProfile/EditProfile.jsx
+++ src/components/userSettings/EditProfile/EditProfile.jsx
@@ -37,21 +37,7 @@ const EditProfile = () => {
   return (
     <ContentWrapper>
       <div className="settings-page-container">
-        <div className="side-nav-container">
-          <ul className="settings-options-list">
-            <li>Public profile</li>
-            <li>Personal information</li>
-            <li>Account Manangement</li>
-            <li>Tune your home feed</li>
-          </ul>
-        </div>
-        <div className="page-content-container">
-          <div className="title-container">
-            <h1>Public Profile</h1>
-            <p>People visiting your profile will see the following info</p>
-          </div>
           <div className="manage-photo-container">
-            <div className="photo-title">Photo</div>
             <div className="photo-n-btn-container">
               <div className="photo-example">
                 <TempUserImage user={user} />
