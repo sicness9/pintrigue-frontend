1b3e4c0d350ee1cdfff34eb07b5dfeb5b9d67322
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-09 19:46:04 -0400

    add api slice to handle all pin queries

---
+++ src/slices/pinApiSlice.js
@@ -0,0 +1,32 @@
+import { apiSlice } from "../app/api/apiSlice";
+
+export const pinApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    addPin: builder.mutation({
+      query: (formData) => ({
+        url: "/pins/",
+        method: "POST",
+        body: formData,
+      }),
+    }),
+    getAllPins: builder.mutation({
+      query: () => ({
+        url: "/pins/all-pins",
+        method: "GET",
+      }),
+    }),
+    searchPin: builder.mutation({
+      query: ({ category, posted_by, page }) => ({
+        url: `/pins/search`,
+        method: "GET",
+        params: { category, posted_by, page },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useAddPinMutation,
+  useGetAllPinsMutation,
+  useSearchPinMutation,
+} = pinApiSlice;
