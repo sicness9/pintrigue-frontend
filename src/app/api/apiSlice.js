76882114cee919ae1a33205bf016c001dd52b372
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-09 19:40:05 -0400

    replace url with vite env vars

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../slices/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8000/api",
+  baseUrl: import.meta.env.VITE_API_URL,
   // credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().token.value;
