4ddf245355d07a3de0bda37df95ad142ca58758d
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-09 19:42:29 -0400

    use search pin mutation to query and replace axios

--- src/components/miscPages/SearchResultGrid/SearchResultGrid.jsx
+++ src/components/miscPages/SearchResultGrid/SearchResultGrid.jsx
@@ -14,22 +14,21 @@ const SearchResultGrid = (props) => {
   const [items, set] = useState([]); // Data will be the query I run to get the data
   const [category, setCategory] = useState(props.category);
   const [posted_by, setPosted_by] = useState("");
+  const [searchPin, { isLoading }] = useSearchPinMutation();
   let page = 0;
 
   console.log("Data:", items);
 
-  const loadMorePins = () => {
-    axios
-      .get("http://127.0.0.1:8000/api/pins/search/", {
-        params: {
-          category: category,
-          posted_by: posted_by,
-          page: page,
-        },
-      })
+  const loadMorePins = async () => {
+    await searchPin({
+      category,
+      posted_by,
+      page,
+    })
+      .unwrap()
       .then((res) => {
         const newPins = [];
-        res.data.pins.forEach((pin) => {
+        res.pins.forEach((pin) => {
           newPins.push(pin);
         });
         set((prevPins) => [...prevPins, ...newPins]);
