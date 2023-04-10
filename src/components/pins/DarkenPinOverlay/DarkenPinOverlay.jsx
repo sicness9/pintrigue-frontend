7b5092b058b15eebe883549a659a728d167fc859
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-10 17:24:16 -0400

    create component to darken pin on hover

---
+++ src/components/pins/DarkenPinOverlay/DarkenPinOverlay.jsx
@@ -0,0 +1,15 @@
+// component
+import SaveButton from "../SaveButton/SaveButton";
+
+// styles
+import { OverlayStyle } from "./DarkenPinOverlay.styles";
+
+const DarkenPinOverlay = () => {
+  return (
+    <OverlayStyle>
+      <SaveButton />
+    </OverlayStyle>
+  );
+};
+
+export default DarkenPinOverlay;
