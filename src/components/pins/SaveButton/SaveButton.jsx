fe24610e5c55f205ce5e5f931e87066676aba9a7
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-10 17:23:52 -0400

    create save button component

---
+++ src/components/pins/SaveButton/SaveButton.jsx
@@ -0,0 +1,11 @@
+import { SaveButtonStyles } from "./SaveButton.styles";
+
+const SaveButton = () => {
+  return (
+    <SaveButtonStyles>
+      <div className="btn-text">Save</div>
+    </SaveButtonStyles>
+  );
+};
+
+export default SaveButton;
