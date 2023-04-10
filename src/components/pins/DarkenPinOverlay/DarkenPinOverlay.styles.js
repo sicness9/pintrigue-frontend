00d3c493857efbf78199df459d0a888788f7f039
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-10 17:24:25 -0400

    add styles for darken component

---
+++ src/components/pins/DarkenPinOverlay/DarkenPinOverlay.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const OverlayStyle = styled.div`
+  position: absolute;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5));
+  z-index: 500;
+  height: 100%;
+  width: 100%;
+`;
