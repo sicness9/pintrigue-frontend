f5b72560a7a34e1567a70db4ea30a077231fc0ce
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-12 16:40:53 -0400

    create button that is shown after pin is saved

---
+++ src/components/pins/UnsaveButton/UnsaveButton.jsx
@@ -0,0 +1,12 @@
+// styles
+import { UnsaveButtonStyles } from "./UnsaveButton.styles";
+
+const UnsaveButton = () => {
+  return (
+    <UnsaveButtonStyles>
+      <div className="btn-text">Saved</div>
+    </UnsaveButtonStyles>
+  );
+};
+
+export default UnsaveButton;
