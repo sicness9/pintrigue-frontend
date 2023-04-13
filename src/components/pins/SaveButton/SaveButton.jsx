380e8c4b3c59dcab39c192b155dc6d16d81a0b34
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-13 16:41:20 -0400

    import mutations and get api query functions

--- src/components/pins/SaveButton/SaveButton.jsx
+++ src/components/pins/SaveButton/SaveButton.jsx
@@ -3,6 +3,15 @@ import { useSelector } from "react-redux";
 // styles
 import { SaveButtonStyles } from "./SaveButton.styles";
 
+// mutations
+import {
+  useAddSaveMutation,
+  useGetAllSavesMutation,
+} from "../../../slices/saveApiSlice";
+
+const SaveButton = (props) => {
+  const [addSave] = useAddSaveMutation();
+  const [getAllSaves] = useGetAllSavesMutation();
   const currentUser = useSelector((state) => state.user.value);
   return (
     <SaveButtonStyles>
