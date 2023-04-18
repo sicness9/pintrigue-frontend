f370f7e102054d4c157e17235eb5944aaeae7120
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-18 15:29:37 -0400

    add styling for new containers and items added to comments section

--- src/pages/ViewPinPage/CommentSection/CommentSection.styles.js
+++ src/pages/ViewPinPage/CommentSection/CommentSection.styles.js
@@ -6,8 +6,7 @@ export const CommentSectionContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  margin-top: 17px;
+  justify-content: start;
 
   .comments-header-container {
     width: 100%;
@@ -25,6 +24,8 @@ export const CommentSectionContainer = styled.div`
 
     .hide-n-show-button-container {
       height: 100%;
+      width: 40px;
+      top: 0;
       display: flex;
       flex-direction: row;
       align-items: center;
@@ -32,7 +33,7 @@ export const CommentSectionContainer = styled.div`
       border-radius: 50%;
 
       &:hover {
-        background-color: #e2e2e2;
+        background-color: var(--greyHover);
       }
 
       .hide-n-show-button {
@@ -53,7 +54,7 @@ export const CommentSectionContainer = styled.div`
         }
 
         .hide-comment-image {
-          width: 18px;
+          width: 16px;
           cursor: pointer;
 
           &:hover {
@@ -66,11 +67,16 @@ export const CommentSectionContainer = styled.div`
 
   .comments-container {
     width: 100%;
-    height: 100%;
+    height: 425px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
+    overflow-y: auto;
+
+    @media screen and (max-width: 1250px) {
+      height: 250px;
+    }
 
     .comment {
       width: 100%;
@@ -82,7 +88,6 @@ export const CommentSectionContainer = styled.div`
       padding-bottom: 10px;
 
       .poster_image {
-        /* width: 100%; */
         display: flex;
         flex-direction: row;
         align-items: center;
@@ -103,13 +108,63 @@ export const CommentSectionContainer = styled.div`
         width: 100%;
         height: 100%;
         display: flex;
-        flex-direction: row;
-        align-items: center;
+        flex-direction: column;
+        align-items: start;
         justify-content: flex-start;
+
+        .comment-user-container {
+        }
 
         .commenter-name {
           font-weight: bold;
           margin-right: 5px;
+        }
+
+        .comment-detail-functions {
+          display: flex;
+          gap: 15px;
+          justify-content: center;
+          align-items: center;
+
+          .post-date {
+            font-size: 14px;
+            color: grey;
+          }
+
+          .reply-btn {
+            border: none;
+            background-color: transparent;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+
+            .reply-btn-text {
+              font-weight: 600;
+              color: grey;
+            }
+          }
+
+          .comment-like-btn {
+            border: none;
+            background-color: transparent;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: row;
+            gap: 3px;
+            cursor: pointer;
+
+            img {
+              width: 15px;
+            }
+
+            .comment-likes {
+              font-size: 14px;
+              color: grey;
+            }
+          }
         }
       }
     }
