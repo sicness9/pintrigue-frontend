f47a07a2a9bce91678c06497881698069b2309d9
sicness9 <30636804+sicness9@users.noreply.github.com>
2023-04-18 15:30:41 -0400

    add more spacing to bottom of component and set max height

--- src/pages/ViewPinPage/PinViewSmall/PinViewSmall.styles.js
+++ src/pages/ViewPinPage/PinViewSmall/PinViewSmall.styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const PinViewSmallStyles = styled.div`
-  margin: 85px auto 10px auto;
+  margin: 85px auto 20px auto;
   display: none;
   box-shadow: rgb(0, 0, 0, 0.5) 0px 3px 10px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 900px;
+  max-height: 1250px;
   width: 800px;
   border-radius: 30px;
   overflow: hidden;
